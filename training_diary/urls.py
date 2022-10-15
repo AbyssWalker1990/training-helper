ba036b513f657376a7f76c0e7587f2e2aa5ec09f
Vova <bordiian.v@gmail.com>
2022-10-15 09:23:24 +0300

    added base views and pages for training_diary app

--- training_diary/urls.py
+++ training_diary/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.main_page, name="main_page")
+    path('', views.main_page, name="main_page"), # in main templates
+    # Rework with pk later
+    path('trainings', views.all_user_trainings, name="all-user-trainings"),
+    path('training-single', views.user_single_training, name="training-single")
+
 ]
--- training_diary/views.py
+++ training_diary/views.py
@@ -4,3 +4,11 @@ from django.shortcuts import render
 
 def main_page(request):
     return render(request, 'main_page.html')
+
+
+def all_user_trainings(request): # Need to add pk later
+    return render(request, 'training_diary/trainings.html')
+
+
+def user_single_training(request):
+    return render(request, 'training_diary/training-single.html')
