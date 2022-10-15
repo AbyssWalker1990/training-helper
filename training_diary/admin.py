028b81124fc3d2756537174a9c1984270bf3419c
Vova <bordiian.v@gmail.com>
2022-10-15 10:05:29 +0300

    created Training and other models for testing

--- training_diary/admin.py
+++ training_diary/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Training, Exercise, Set
 
 # Register your models here.
+
+admin.site.register(Training)
+admin.site.register(Exercise)
+admin.site.register(Set)
+
--- training_diary/models.py
+++ training_diary/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from django.contrib.auth.models import User
+import uuid
 
 # Create your models here.
+
+
+
+class Training(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=300)
+    description = models.CharField(max_length=800, blank=True, null=True)
+    date = models.DateTimeField()
+
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+
+    def __str__(self):
+        return str(self.name)
+
+
+class Exercise(models.Model):
+    exercise_name = models.CharField(max_length=150)
+    training = models.ForeignKey(Training, on_delete=models.CASCADE)
+
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+
+    def __str__(self):
+        return str(self.exercise_name)
+
+class Set(models.Model):
+    reps = models.IntegerField()
+    weight = models.FloatField()
+    exercise = models.ForeignKey(Exercise, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+
+    def __str__(self):
+        return f"{self.reps}*{self.weight}"
+
