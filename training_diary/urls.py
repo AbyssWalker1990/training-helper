d9dfae24b3c1c5553257d6952e808f394b05dc1a
Vova <bordiian.v@gmail.com>
2022-10-15 17:17:42 +0300

    basic functionality for trainings and single-training pages(output basic info) DONE

--- training_diary/urls.py
+++ training_diary/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.main_page, name="main_page"), # in main templates
     # Rework with pk later
-    path('trainings', views.all_user_trainings, name="all-user-trainings"),
-    path('training-single', views.user_single_training, name="training-single")
+    path('trainings/<str:pk>', views.all_user_trainings, name="all-user-trainings"),
+    path('training-single/<str:pk>', views.user_single_training, name="training-single")
 
 ]
--- training_diary/views.py
+++ training_diary/views.py
@@ -1,14 +1,22 @@
 from django.shortcuts import render
-
+from .models import Training, Exercise, Set
+from django.contrib.auth.models import User
 # Create your views here.
 
 def main_page(request):
     return render(request, 'main_page.html')
 
 
-def all_user_trainings(request): # Need to add pk later
-    return render(request, 'training_diary/trainings.html')
+def all_user_trainings(request, pk): # Need to add pk later
+    user = User.objects.get(id=pk)
+    print('USER = ', user)
+    trainings = Training.objects.filter(user=user)
+    context = {'trainings': trainings}
+    return render(request, 'training_diary/trainings.html', context)
 
 
-def user_single_training(request):
-    return render(request, 'training_diary/training-single.html')
+def user_single_training(request, pk):
+    current_training = Training.objects.get(id=pk)
+    exercises = current_training.exercise_set.all()
+    context = {'exercises': exercises}
+    return render(request, 'training_diary/training-single.html', context)
